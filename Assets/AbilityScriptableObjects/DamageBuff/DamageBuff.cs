908ade4952dab323b9a2f97112c568180265f4ce
Igor Bessa <ibessa@ucsc.edu>
2025-01-18 10:59:58 -0800

    pushing unsaved changes to DamageBuff.cs

--- Assets/AbilityScriptableObjects/DamageBuff/DamageBuff.cs
+++ Assets/AbilityScriptableObjects/DamageBuff/DamageBuff.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/DamageBuff")]
 public class DamageBuff : AbilityAbstractClass
 {
-    GameObject player = PlayerManager.instance.player;
+    private GameObject player;
     [SerializeField] public int buffAmount;
     
     public override void Initialize(int duration)
     {
+        player = PlayerManager.instance.player;
+
         // TODO: make buffAmount based on soupValue
-        // buffAmount = Mathf.CeilToInt(PlayerManager.instance.soupVal / 25)
+        // ex: buffAmount = Mathf.CeilToInt(PlayerManager.instance.soupVal / 25)
+
         if (player != null)
         {
             player.GetComponent<PlayerAttack>().playerDamage += buffAmount;
             // TODO: make maxUsage based on soupValue
-            // _maxUsage = soupVal / 10
+            // ex: _maxUsage = soupVal / 10
             _remainingUsage = _maxUsage;
             Debug.Log("buffing player!");
         }
