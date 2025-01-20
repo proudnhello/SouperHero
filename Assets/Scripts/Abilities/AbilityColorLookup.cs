d563e54d9c6df608da6940caa1ef3b2b0f9eaa23
Igor Bessa <ibessa@ucsc.edu>
2025-01-19 21:49:28 -0800

    Updated color lookup table

--- Assets/Scripts/Abilities/AbilityColorLookup.cs
+++ Assets/Scripts/Abilities/AbilityColorLookup.cs
@@ -1,38 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-// Dictionary containing an ability name and its corresponding color for UI
-// ability names will be the strings determined in the CreateAssetMenu for each ability
-// colors will be hex values for the UI to use 
-public class AbilityColorLookup : MonoBehaviour
+[CreateAssetMenu(menuName = "AbilityColors/LookupTable")]
+public class AbilityColorLookup : ScriptableObject
 {
-    // Start is called before the first frame update
-    public Dictionary <string, string> abilityColorLookup;
+    [Serializable]
 
-    // get reference to soup manager/all created abilities
-    void Start()
+    public struct AbilityColorLookupEntry
     {
+        public string color;                        // string containing a hex value
+        public string enemyName;                    // enemy associated with the color
+    }
+    public AbilityColorLookupEntry[] lookup;
+    private Dictionary<string, string> abilityColorLookup;
+    
+    public void CreateColorLookup()
+    {
+        abilityColorLookup = new Dictionary<string, string>();
+        foreach (AbilityColorLookupEntry entry in lookup)
+        {
+            abilityColorLookup.Add(enemyName, entry.color);
+        }
+    }   
 
-        // Once soup manager is implemented, abilities will be pulled from there dynamically,
-        // for now they are hardcoded 
-
-        // Psuedocode: 
-        // foreach (ability in soupManager.abilities)
-        // {
-        //     abilityColorLookup.Add(ability.name, ability.color);
-        // }
-        // ability colors might just be defined in this file ? confirm later 
-
-        abilityColorLookup = new Dictionary<string, string>(); 
-        abilityColorLookup.Add("Damage Up", "#3b00a1");
-        abilityColorLookup.Add("Fireball", "e31300");
+    public string GetColor(string enemyName)
+    {
+        return abilityColorLookup[enemyName];
     }
 
-    public string GetColor(string abilityName)
-    {
-        // returns the hex color value for the given ability
-        return abilityColorLookup[abilityName];
-    }
 }
