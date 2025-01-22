755856223f24990da9571fbef1c608493be865e6
Igor Bessa <ibessa@ucsc.edu>
2025-01-21 17:42:19 -0800

    Player flashes when taking damage

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    [SerializeField] private float damageTime = 1.0f;   // time the player flashes red when taking damage
+    private bool takingDamage = false;
     void Start()
     {
         PlayerManager.instance.SetHealth(PlayerManager.instance.GetMaxHealth());
@@ -16,9 +19,55 @@ public class PlayerHealth : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 7)    // 7 is the layer for enemy
+        if (collision.gameObject.layer == 7 && !takingDamage)    // 7 is the layer for enemy
         {
-            PlayerManager.instance.TakeDamage(10);
+            takingDamage = true;
+            KnockBack(collision.gameObject);
+            StartCoroutine("TakeDamage");
         }
     }
+
+    private void KnockBack(GameObject collider )
+    {
+        Debug.Log("Knocking back player");
+        PlayerManager.instance.TakeDamage(10); // change this so that the player takes damage based on the enemy's damage
+        Vector3 direction = (PlayerManager.instance.player.transform.position - collider.transform.position).normalized;
+        PlayerManager.instance.player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        PlayerManager.instance.player.GetComponent<Rigidbody2D>().AddForce(direction * 3, ForceMode2D.Impulse);
+    }
+
+    public IEnumerator TakeDamage(){
+        Debug.Log("Flashing damage indicator");
+
+        int maxFlashCycles = Mathf.CeilToInt((damageTime / 0.3f));
+        int flashCycles = 0;
+        Color playerColor = PlayerManager.instance.player.GetComponent<SpriteRenderer>().color;
+        
+        while(maxFlashCycles > flashCycles)
+        {
+            PlayerManager.instance.player.GetComponent<SpriteRenderer>().color = Color.red;
+            yield return new WaitForSeconds(0.15f);
+            PlayerManager.instance.player.GetComponent<SpriteRenderer>().color = playerColor;
+            yield return new WaitForSeconds(0.15f);
+            flashCycles++;
+        }
+        takingDamage = false;
+    }
+
+    /*
+    public IEnumerator KnockBack()
+    {
+        int maxFlashCycles = Mathf.CeilToInt((knockBackTime / 0.3f));
+        int flashCycles = 0;
+        while(maxFlashCycles > flashCycles)
+        {
+            sprite.color = Color.red;
+            yield return new WaitForSeconds(0.15f);
+            sprite.color = _initialColor;
+            yield return new WaitForSeconds(0.15f);
+            flashCycles++;
+        }
+        takingDamage = false;
+    } 
+    */
 }
