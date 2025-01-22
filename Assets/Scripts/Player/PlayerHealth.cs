7dd9115684ff8861faf3ffa3200fcba331e2469b
Igor Bessa <ibessa@ucsc.edu>
2025-01-21 18:18:59 -0800

    Taking damage for no reason and knockback not working, working on a fix

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,7 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
 
     [SerializeField] private float damageTime = 1.0f;   // time the player flashes red when taking damage
-    private bool takingDamage = false;
+    private bool invincible = false;
     void Start()
     {
         PlayerManager.instance.SetHealth(PlayerManager.instance.GetMaxHealth());
@@ -19,27 +19,25 @@ public class PlayerHealth : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 7 && !takingDamage)    // 7 is the layer for enemy
+        if (collision.gameObject.tag == "Enemy" && !invincible)    
         {
-            takingDamage = true;
+            invincible = true;
+            PlayerManager.instance.TakeDamage(10); // change this so that the player takes damage based on the enemy's damage
             KnockBack(collision.gameObject);
             StartCoroutine("TakeDamage");
         }
     }
 
-    private void KnockBack(GameObject collider )
+    private void KnockBack(GameObject collider)
     {
-        Debug.Log("Knocking back player");
-        PlayerManager.instance.TakeDamage(10); // change this so that the player takes damage based on the enemy's damage
-        Vector3 direction = (PlayerManager.instance.player.transform.position - collider.transform.position).normalized;
+        Vector3 direction = (PlayerManager.instance.player.GetComponent<Transform>().position - collider.GetComponent<Transform>().position).normalized;
         PlayerManager.instance.player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         PlayerManager.instance.player.GetComponent<Rigidbody2D>().AddForce(direction * 3, ForceMode2D.Impulse);
     }
 
     public IEnumerator TakeDamage(){
-        Debug.Log("Flashing damage indicator");
 
-        int maxFlashCycles = Mathf.CeilToInt((damageTime / 0.3f));
+        float maxFlashCycles = ((damageTime / 0.3f));
         int flashCycles = 0;
         Color playerColor = PlayerManager.instance.player.GetComponent<SpriteRenderer>().color;
         
@@ -51,23 +49,6 @@ public class PlayerHealth : MonoBehaviour
             yield return new WaitForSeconds(0.15f);
             flashCycles++;
         }
-        takingDamage = false;
+        invincible = false;
     }
-
-    /*
-    public IEnumerator KnockBack()
-    {
-        int maxFlashCycles = Mathf.CeilToInt((knockBackTime / 0.3f));
-        int flashCycles = 0;
-        while(maxFlashCycles > flashCycles)
-        {
-            sprite.color = Color.red;
-            yield return new WaitForSeconds(0.15f);
-            sprite.color = _initialColor;
-            yield return new WaitForSeconds(0.15f);
-            flashCycles++;
-        }
-        takingDamage = false;
-    } 
-    */
 }
