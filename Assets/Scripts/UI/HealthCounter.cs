5d45e70e80670ebd76060f116ea62230df50aea1
Charlize <extrabutterychar@gmail.com>
2025-01-23 14:35:55 -0800

    Updating health counter and set temporarily to a divisible by 10 rate, also removed UI Manager health implementation -- sprint1testscene

--- Assets/Scripts/UI/HealthCounter.cs
+++ Assets/Scripts/UI/HealthCounter.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using UnityEngine;
 
-public class HealthCounter : MonoBehaviour
+public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("UI Configuration")]
+    [SerializeField] List<GameObject> heartList;
+    private int heartCount = 0;
+    private int playerHealth = PlayerManager.instance.health / 10;
+    [Header("Debug")]
+    public GameObject healthText;
+
+    void Start() {
+        // Initialize with all hearts as empty
+        foreach(GameObject heart in heartList) {
+            heart.SetActive(false);
+        }
+
+        for (int i = 0; i < playerHealth; i++) {
+            heartList[i].SetActive(true);
+            heartCount++;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HealthChange(); 
+    }
+
+    public void HealthChange() {
+        healthText.GetComponent<TMP_Text>().text = PlayerManager.instance.health.ToString();
+        playerHealth = PlayerManager.instance.health / 10;
+        if (heartCount < playerHealth - 1) {
+            AddHealth();
+        }
+        if (heartCount > playerHealth - 1) {
+            RemoveHealth();
+        }
+    }
+
+    void AddHealth() {
+        Debug.Log("Health Count: " + heartCount + "Player Health " + playerHealth);
+        heartList[heartCount].SetActive(true);
+        heartCount++;
+
+    }
+
+    void RemoveHealth() {
+        heartList[heartCount].SetActive(false);
+        heartCount--;
+
     }
 }
