0872e4ab4e605179a97c63f439ace33cf431e3a6
Charlize <extrabutterychar@gmail.com>
2025-01-26 18:15:12 -0800

    Pushing current work, UI manager changes able to change color but needs enemy colors, set up for room generation, set up prefab for room for spawners, preparing to obtain enemy effects, and fix Brian spawner - charlizescene

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,12 +7,41 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
+    [SerializeField] private GameObject soupPot;
+    private static SpoonCounter spoonCounter;
+    public int playerSpoons;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
+    }
+    void Start() {
+        spoonCounter = soupPot.GetComponent<SpoonCounter>();
+    }
+
+    void Update() {
+        UpdateAbilities();
+        // int newSpoonCount = PlayerManager.instance.GetAbilities().Count;
+        // UpdateSpoons(newSpoonCount);
+    }
+
+    void UpdateSpoons(int count) {
+        if (playerSpoons < count) {
+            spoonCounter.AddSpoon(Color.red, playerSpoons);
+            playerSpoons++;
+        }
+        if (playerSpoons > count) {
+            spoonCounter.DeleteSpoon(playerSpoons);
+            playerSpoons--;
+        }
+    }
+
+    public void UpdateAbilities() {
+        int newSpoonCount = PlayerManager.instance.GetAbilities().Count;
+        UpdateSpoons(newSpoonCount);
     }
 
 }
