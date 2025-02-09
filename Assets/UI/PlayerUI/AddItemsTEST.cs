cdeb421e28688cc85efa8dd274516f0ac16514d4
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-09 00:00:40 -0800

    Testing
    My initial test to get a prefab to drop into the pot in the UI layer. Issues with the canvas parent and trying to align the prefab.
    This isn't working :(

---
+++ Assets/UI/PlayerUI/AddItemsTEST.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddItemsTEST : MonoBehaviour
+{
+
+    public GameObject ingredientObj;
+    public GameObject potObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(ingredientObj, new Vector2(potObj.transform.position.x - 50.0f, potObj.transform.position.y + 85.0f), Quaternion.identity, this.transform);
+        //Instantiate(ingredientObj, potObj.transform.position, Quaternion.identity, this.transform);
+        Instantiate(ingredientObj, new Vector2(0,0), Quaternion.identity, this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
