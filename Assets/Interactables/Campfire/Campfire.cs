bb4f69935c474e8a4b29d5b0f7c98c4f81c90efe
Igor Bessa <ibessa@ucsc.edu>
2025-02-08 22:45:44 -0800

    temporary functionality for campfire working

--- Assets/Interactables/Campfire/Campfire.cs
+++ Assets/Interactables/Campfire/Campfire.cs
@@ -7,9 +7,12 @@ public class Campfire : Interactable
 {
     [Header("Campfire")]
     [SerializeField] private GameObject cookingPot; 
+    [SerializeField] private GameObject CookingScreen; 
+    private float cookSpeed = 8f;
+    private bool cooking = false;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         type = this.name;
         SetInteractable(true);
@@ -21,8 +24,38 @@ public class Campfire : Interactable
     {
         if (CanInteract())
         {
+            cooking = true;
             cookingPot.SetActive(true);
-            Debug.Log("Cooking at campfire");
+            CookingScreen.SetActive(true);
         }
+    }
+
+    // ---- WIP ----
+    // private void AnimatePotMovement()
+    // {
+    //     Debug.Log("Animating pot movement");
+    //     cookingPot.SetActive(true);
+    //     // animate the pot moving from the player to the campfire
+    //     Vector2 playerPosition = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
+    //     cookingPot.transform.position = playerPosition;
+    //     cookingPot.transform.position = Vector2.MoveTowards(cookingPot.transform.position, this.transform.position, cookSpeed * Time.deltaTime);
+    // }
+
+    private void Update()
+    {
+
+        // if (cooking)
+        // {
+        //     AnimatePotMovement();
+        // }
+
+        if (!interactablePrompt.activeSelf)
+        {
+            cookingPot.SetActive(false);
+            CookingScreen.SetActive(false);
+            cooking = false;
+        }
+
+
     }
 }
