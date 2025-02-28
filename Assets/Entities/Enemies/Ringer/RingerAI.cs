84a47cbdef7d0599258bdc16c66db0984b137120
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-02-28 15:07:55 -0800

    ringer flees from player (looks wierd)

--- Assets/Entities/Enemies/Ringer/RingerAI.cs
+++ Assets/Entities/Enemies/Ringer/RingerAI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
+using static Entity;
 
 //The ranged enemy follows player constantly until they are in range, at which point they freeze and fire bullets
 //Code tutorial for ranged enemy found at: https://www.youtube.com/watch?v=bwi4lteomic
@@ -23,25 +24,130 @@ public class RingerAI : EnemyBaseClass
     [SerializeField] private float timeToShoot = 0.5f;
     [SerializeField] private float timeAfterShoot = 0.5f;
     [SerializeField] private float timeBetweenShots = 2.0f;
-    private float windUp = 0.0f;
-    private float timeSinceLastShot = 0.0f;
     public Transform firingPoint;
     public HopShroomSpore bullet;
     private Animator animator;
 
-    RingerState state;
+    private RingerState currentState;
     RingerState idle;
     RingerState chasing;
     RingerState shooting;
 
-    private abstract class RingerState
+    public abstract class RingerState
     {
-        protected RingerAI ringer;
-        abstract public void Update(RingerAI ringer);
+        abstract public void Update(RingerAI ringer, float deltaT);
         abstract public void Enter(RingerAI ringer);
         abstract public void Exit(RingerAI ringer);
     }
 
+    public class IdleState : RingerState
+    {
+        public override void Enter(RingerAI ringer)
+        {
+            ringer.agent.isStopped = true;
+        }
+
+        public override void Exit(RingerAI ringer)
+        {
+            ringer.agent.isStopped = false;
+        }
+
+        public override void Update(RingerAI ringer, float deltaT)
+        {
+            if (ringer.playerDetected)
+            {
+                ringer.currentState.Exit(ringer);
+                ringer.currentState = ringer.chasing;
+                ringer.currentState.Enter(ringer);
+            }
+            else
+            {
+                ringer.agent.SetDestination(ringer.transform.position);
+            }
+        }
+    }
+
+    public class ChasingState : RingerState
+    {
+        float waitTillShoot = 0.0f;
+        public override void Enter(RingerAI ringer)
+        {
+            waitTillShoot = ringer.timeBetweenShots;
+            ringer.agent.isStopped = false;
+        }
+
+        public override void Exit(RingerAI ringer)
+        {
+            ringer.agent.isStopped = true;
+        }
+
+        public override void Update(RingerAI ringer, float deltaT)
+        {
+            waitTillShoot -= deltaT;
+            float distance = Vector2.Distance(ringer.transform.position, PlayerEntityManager.Singleton.GetPlayerPosition());
+            if(distance < ringer.shootingRadius && waitTillShoot <= 0)
+            {
+                waitTillShoot = ringer.timeBetweenShots;
+                //ringer.currentState.Exit(ringer);
+                //ringer.currentState = ringer.shooting;
+                //ringer.currentState.Enter(ringer);
+            }
+            else if(distance > ringer.followingRadius)
+            {
+                ringer.currentState.Exit(ringer);
+                ringer.currentState = ringer.idle;
+                ringer.currentState.Enter(ringer);
+            }
+            else if (distance < ringer.fleeRadius)
+            {
+                ringer.agent.speed = ringer.GetMoveSpeed() * 2;
+                Vector2 holder = ringer.transform.position;
+                Vector2 awayFromPlayer = holder - PlayerEntityManager.Singleton.GetPlayerPosition();
+                awayFromPlayer.Normalize();
+                ringer.agent.SetDestination(holder + (awayFromPlayer * 3));
+            }
+            else
+            {
+                ringer.agent.speed = ringer.GetMoveSpeed();
+                ringer.agent.SetDestination(PlayerEntityManager.Singleton.GetPlayerPosition());
+            }
+        }
+    }
+
+    public class ShootingState : RingerState
+    {
+        float waitTillShoot = 0.0f;
+        public override void Enter(RingerAI ringer)
+        {
+            waitTillShoot = ringer.timeToShoot;
+            ringer.agent.isStopped = true;
+        }
+
+        public override void Exit(RingerAI ringer)
+        {
+            ringer.agent.isStopped = false;
+        }
+
+        public override void Update(RingerAI ringer, float deltaT)
+        {
+            print("IMMA FIRING MY LAZERS");
+            return;
+            waitTillShoot -= deltaT;
+            if(waitTillShoot <= 0)
+            {
+                // ringer.Shoot();
+                waitTillShoot = ringer.timeBetweenShots;
+            }
+            float distance = Vector2.Distance(ringer.transform.position, PlayerEntityManager.Singleton.GetPlayerPosition());
+            if(distance > ringer.shootingRadius)
+            {
+                ringer.currentState.Exit(ringer);
+                ringer.currentState = ringer.chasing;
+                ringer.currentState.Enter(ringer);
+            }
+        }
+    }
+    
 
     protected void Start()
     {
@@ -50,11 +156,19 @@ public class RingerAI : EnemyBaseClass
         agent.updateUpAxis = false;
         agent.speed = GetMoveSpeed();
 
+        idle = new IdleState();
+        chasing = new ChasingState();
+        shooting = new ShootingState();
+
+        currentState = idle;
+
+        StartCoroutine(DetectionCoroutine());
     }
 
     protected override void UpdateAI()
     {
-        return;
+        print(currentState.GetType());
+        currentState.Update(this, Time.deltaTime);
     }
 
     IEnumerator DetectionCoroutine()
