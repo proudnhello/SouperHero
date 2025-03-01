353af83f4e7a09289c16fc41ded7ca0ec4de2dce
lweislak <150990328+lweislak@users.noreply.github.com>
2025-03-01 02:15:55 -0800

    Small edit

--- Assets/Ingredients/Abilities/ProjectileAbilities/Projectile/DoubleProjectile.cs
+++ Assets/Ingredients/Abilities/ProjectileAbilities/Projectile/DoubleProjectile.cs
@@ -15,7 +15,10 @@ public class DoubleProjectile : AbilityAbstractClass
     [SerializeField] float CRIT_MULTIPLIER = 0.5f;
     [SerializeField] float SPEED_MULTIPLIER = 0.5f;
 
-    // Spawn projectile at player's position, and then set its rotation to be facing the same direction as the player
+    private float offset = 0.2f;
+
+    //Get a projectile based on the direction a player is facing,
+    //face the projectile in the direction of the target angle and spawn it
     public override void UseAbility(AbilityStats stats, List<Infliction> inflictions)
     {
 
@@ -24,17 +27,16 @@ public class DoubleProjectile : AbilityAbstractClass
 
         Vector2 currDir = PlayerEntityManager.Singleton.playerAttackPoint.transform.up;
 
+        //First projectile
         ProjectileObject proj1 = spawner.GetProjectile();
-        float targetAngle = Mathf.Atan2(currDir.y, currDir.x) - 0.2f;
-
-
+        float targetAngle = Mathf.Atan2(currDir.y, currDir.x) - offset;
         Vector2 newDir = new Vector2(Mathf.Cos(targetAngle), Mathf.Sin(targetAngle));
         proj1.Spawn(PlayerEntityManager.Singleton.playerAttackPoint.position,
             newDir, stats, inflictions);
 
+        //Second projectile
         ProjectileObject proj2 = spawner.GetProjectile();
-        targetAngle = Mathf.Atan2(currDir.y, currDir.x) + 0.2f;
-
+        targetAngle = Mathf.Atan2(currDir.y, currDir.x) + offset;
         newDir = new Vector2(Mathf.Cos(targetAngle), Mathf.Sin(targetAngle));
         proj2.Spawn(PlayerEntityManager.Singleton.playerAttackPoint.position,
             newDir, stats, inflictions);
