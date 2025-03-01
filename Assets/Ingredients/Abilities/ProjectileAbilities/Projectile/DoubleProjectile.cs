5f1048f3758d7778dbb41835338778c99139d009
lweislak <150990328+lweislak@users.noreply.github.com>
2025-03-01 01:48:42 -0800

    Working double projectile
    Ingredient that contains the double projectile (Fire Biscuit) is attached to the Habaperro.
    Calculate new direction: current direction player is facing + offset.
    Must spawn first projectile before getting second.

--- Assets/Ingredients/Abilities/ProjectileAbilities/Projectile/DoubleProjectile.cs
+++ Assets/Ingredients/Abilities/ProjectileAbilities/Projectile/DoubleProjectile.cs
@@ -5,6 +5,7 @@ using Infliction = SoupSpoon.SpoonInfliction;
 
 // To create a projectile scriptable object, just go to the project overview, right click, click "create", and
 // at the top should be a menu titled "abilities". Should be under that menu.
+
 [CreateAssetMenu(menuName = "Abilities/DoubleProjectile")]
 public class DoubleProjectile : AbilityAbstractClass
 {
@@ -14,25 +15,28 @@ public class DoubleProjectile : AbilityAbstractClass
     [SerializeField] float CRIT_MULTIPLIER = 0.5f;
     [SerializeField] float SPEED_MULTIPLIER = 0.5f;
 
+    // Spawn projectile at player's position, and then set its rotation to be facing the same direction as the player
     public override void UseAbility(AbilityStats stats, List<Infliction> inflictions)
     {
 
-        //Debug.Log($"Is spawner null?: {spawner}");
-        // Spawn projectile at player's position, and then set its rotation to be facing the same direction as the player.
-        ProjectileObject proj1 = spawner.GetProjectile();
         stats.size *= SIZE_MULTIPLIER;
         stats.speed *= SPEED_MULTIPLIER;
+
         Vector2 currDir = PlayerEntityManager.Singleton.playerAttackPoint.transform.up;
-        float targetAngle = Mathf.Atan2(currDir.x, currDir.y) - .35f;
-        Vector2 dir = new Vector2(currDir.x * Mathf.Cos(targetAngle) - currDir.y * Mathf.Sin(targetAngle),
-            currDir.x * Mathf.Sin(targetAngle) + currDir.y * Mathf.Cos(targetAngle));
+
+        ProjectileObject proj1 = spawner.GetProjectile();
+        float targetAngle = Mathf.Atan2(currDir.y, currDir.x) - 0.2f;
+
+
+        Vector2 newDir = new Vector2(Mathf.Cos(targetAngle), Mathf.Sin(targetAngle));
         proj1.Spawn(PlayerEntityManager.Singleton.playerAttackPoint.position,
-            dir, stats, inflictions);
-        //ProjectileObject proj2 = spawner.GetProjectile();
-        //proj2.Spawn(PlayerEntityManager.Singleton.playerAttackPoint.position,
-        //    PlayerEntityManager.Singleton.playerAttackPoint.transform.up,
-        //    stats, inflictions);
-        //proj1.transform.Rotate(new Vector3(20f, 0f, 0f));
-        //proj2.transform.Rotate(new Vector3(160f, 0f, 0f));
+            newDir, stats, inflictions);
+
+        ProjectileObject proj2 = spawner.GetProjectile();
+        targetAngle = Mathf.Atan2(currDir.y, currDir.x) + 0.2f;
+
+        newDir = new Vector2(Mathf.Cos(targetAngle), Mathf.Sin(targetAngle));
+        proj2.Spawn(PlayerEntityManager.Singleton.playerAttackPoint.position,
+            newDir, stats, inflictions);
     }
 }
