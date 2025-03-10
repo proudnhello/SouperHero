6c9124c2459839957e1ee181e4562c82e21f1c46
brain.exe <brainfeng135@gmail.com>
2025-03-10 07:56:58 -0700

    update save data and implement it

--- Assets/Helper/SaveManager.cs
+++ Assets/Helper/SaveManager.cs
@@ -5,26 +5,49 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using Unity.VisualScripting;
 using System;
+using Unity.Profiling;
 //Input manager finds the current input system being used
 //In theory, should swap between UI and Player input when pausing the game
 //Right now the input is simply disabled. This is controlled in the GameManager
 public class SaveManager : MonoBehaviour
 {
+    public static SaveManager Singleton { get; private set; }
+    private String saveDataPath = Application.persistentDataPath + "save.dat";
+    public void Awake()
+    {
+        if (Singleton != null && Singleton != this) Destroy(this);
+        else Singleton = this;
+    }
     public void SaveGame(){
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file;
 
-        if(File.Exists(Application.persistentDataPath + "save.dat")){
-            file = File.Open(Application.persistentDataPath + "save.dat", FileMode.Open);
+        if(File.Exists(saveDataPath)){
+            file = File.Open(saveDataPath, FileMode.Open);
         }
         else{
-            file = File.Create(Application.persistentDataPath + "/save.dat");
+            file = File.Create(saveDataPath);
         }
 
         SaveData data = new SaveData();
+        bf.Serialize(file, data);
+        file.Close();
+        Debug.Log("Game saved");
+    }
+
+    public void LoadGame(){
+        if(File.Exists(saveDataPath)){
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(saveDataPath, FileMode.Open);   
+            SaveData data = (SaveData)bf.Deserialize(file);
+
+            // TODO: figure out what to reload
+        }
+
     }
 }
 
+// TODO: figure out what else to save
 [Serializable]
 public class SaveData{
     public List<String> roomEnemies;
