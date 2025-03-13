30872107ef442af92e33426d8a8a1d5e83d67e11
lweislak <150990328+lweislak@users.noreply.github.com>
2025-03-13 01:02:32 -0700

    Highlight current spoon
    Spoons get big when selected.
    
    BUG: Revert back to previous spoon when uses run out

--- Assets/UI/PlayerUI/Spoons/SpoonsEquipped.cs
+++ Assets/UI/PlayerUI/Spoons/SpoonsEquipped.cs
@@ -11,6 +11,9 @@ public class SpoonsEquipped : MonoBehaviour
     [SerializeField] Image[] imageComponents;
     private int prevSpoon = -1;
 
+    private Vector2 normalSize = new Vector2(82, 50);
+    private Vector2 selectedSize = new Vector2(123, 75);
+
     private void Start()
     {
         PlayerInventory.ChangedSpoon += ChangeSpoon;
@@ -19,11 +22,15 @@ public class SpoonsEquipped : MonoBehaviour
 
     void ChangeSpoon(int spoon)
     {
-        if (prevSpoon >= 0)
+        if (prevSpoon >= 0) //Revert changes on previous spoon, except at game start
         {
-            imageComponents[prevSpoon].color = Color.white; //Change current spoon's color
+            imageComponents[prevSpoon].color = Color.white;
+            imageComponents[prevSpoon].rectTransform.sizeDelta = normalSize;
         }
+
+        //Highlight current spoon
         prevSpoon = spoon;
-        imageComponents[spoon].color = new Color(252f/255f, 173f/255f, 3f/255f, 1.0f); //Change current spoon's color
+        imageComponents[spoon].color = new Color(252f/255f, 173f/255f, 3f/255f, 1.0f);
+        imageComponents[prevSpoon].rectTransform.sizeDelta = selectedSize;
     }
 }
