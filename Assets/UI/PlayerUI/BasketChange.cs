f5f64158dc8d150570859c542b849c148d7addc4
Cheristic <ethanheffan@gmail.com>
2025-03-15 13:49:19 -0700

    removes extra debug.log for collider

--- Assets/UI/PlayerUI/BasketChange.cs
+++ Assets/UI/PlayerUI/BasketChange.cs
@@ -4,26 +4,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
-public class BasketChange : MonoBehaviour//, IPointerEnterHandler, IPointerExitHandler
+public class BasketChange : MonoBehaviour
 {
-    //public void OnPointerEnter(PointerEventData eventData)
-    //{
-    //    CursorManager.Singleton.cursorObject.SetActive(false);
-    //    CursorManager.Singleton.ShowCookingCursor();
-    //}
-
-    //public void OnPointerExit(PointerEventData eventData)
-    //{
-    //    if(!CursorManager.Singleton.isDragging)
-    //    {
-    //        CursorManager.Singleton.cursorObject.SetActive(true);
-    //        CursorManager.Singleton.HideCookingCursor();
-    //    }
-    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("collided!");
         if (collision.gameObject.TryGetComponent<DraggableItem>(out DraggableItem dG))
         {
             dG.image.raycastTarget = true;
