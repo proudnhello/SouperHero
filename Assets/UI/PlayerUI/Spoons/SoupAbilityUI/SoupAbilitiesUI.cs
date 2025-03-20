b2341f40fee35e76e8795465cf43ba28d30527cb
Sinfinite01 <54284225+djlouie@users.noreply.github.com>
2025-03-19 23:59:55 -0700

    Update ability text

--- Assets/UI/PlayerUI/Spoons/SoupAbilityUI/SoupAbilitiesUI.cs
+++ Assets/UI/PlayerUI/Spoons/SoupAbilityUI/SoupAbilitiesUI.cs
@@ -1,22 +1,53 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
+using static SoupSpoon;
 
 public class SoupAbilitiesUI : MonoBehaviour
 {
 
     public List<Image> children;
-    public List<TextMeshProUGUI> usesText;
+    public List<TextMeshProUGUI> usesTexts;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Fillout Lists
         foreach(Transform child in transform)
         {
             children.Add(child.GetComponent<Image>());
-            usesText.Add(child.GetChild(0).GetComponent<TextMeshProUGUI>());
+            usesTexts.Add(child.GetChild(0).GetComponent<TextMeshProUGUI>());
+        }
+
+        // Subscribe to event
+        PlayerInventory.ChangedSpoon += UpdateUsesText;
+    }
+
+    void UpdateUsesText(int spoonIdx)
+    {
+        SoupSpoon currentSpoon = PlayerInventory.Singleton.GetSpoons()[spoonIdx];
+
+        // set the uses text for each ability
+        int abilityIdx = 0;
+        foreach (SpoonAbility spoonAbility in currentSpoon.spoonAbilities)
+        {
+            if(spoonAbility.uses == -1)
+            {
+                usesTexts[abilityIdx].text = "∞";
+            } else
+            {
+                usesTexts[abilityIdx].text = spoonAbility.uses.ToString();
+            }
+            
+            abilityIdx++;
+        }
+
+        // set the rest of the texts to none
+        for (int i = abilityIdx; i < CookingManager.Singleton.cookingSlots.Count; i++)
+        {
+            usesTexts[i].text = "";
         }
     }
 
