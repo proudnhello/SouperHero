187d413ffd8a310cef8a8de8f10d767125f424b4
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2025-04-17 23:26:18 -0700

    added code

---
+++ Assets/Helper/CombatTestRoom.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatTestRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject playScreen;
+    public GameObject pauseScreen;
+    public GameObject debugScreen;
+    public KeyCode debugKey = KeyCode.J;
+    public bool debugging = false;
+
+    public List<GameObject> enemyList;
+    public List<GameObject> destroyableList;
+    public List<GameObject> forageableList;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(debugKey) && debugScreen != null 
+            && !CookingManager.Singleton.IsCooking()){
+                debugging = !debugging;
+                if(debugging){
+                    openDebugMenu();
+                } else {
+                    closeDebugMenu();
+                }
+            } 
+            
+    }
+
+    public void openDebugMenu(){
+        debugging = true;
+        Time.timeScale = 0;
+        CursorManager.Singleton.cursorObject.SetActive(false);
+        pauseScreen.SetActive(true);
+        PlayerEntityManager.Singleton.input.Disable();
+    }
+
+    public void closeDebugMenu(){
+
+    }
+}
