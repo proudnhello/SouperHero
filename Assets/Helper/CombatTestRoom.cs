7f43ecb9023193316c52fbad279fcfa896046ce7
Calvin Hunter <calvinhunter03@gmail.com>
2025-04-22 20:36:45 -0700

    added a lot of code

--- Assets/Helper/CombatTestRoom.cs
+++ Assets/Helper/CombatTestRoom.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using TMPro;
 
 public class CombatTestRoom : MonoBehaviour
 {
@@ -17,28 +19,54 @@ public class CombatTestRoom : MonoBehaviour
     public List<GameObject> destroyableList;
     public List<GameObject> forageableList;
 
-    private List<List<GameObject>> objectsToSpawn;
+    private Dictionary<GameObject, int> enemyDict;
+    private Dictionary<GameObject, int> destroyableDict;
+    private Dictionary<GameObject, int> forageableDict;
+
+    public TMP_InputField enemyInputNumber;
+    public TMP_InputField destroyableInputNumber;
+    public TMP_InputField forageableInputNumber;
+
+    public TMP_Dropdown enemyDropdown;
+    public TMP_Dropdown destroyableDropdown;
+    public TMP_Dropdown forageableDropdown;
+
+    public TMP_Text enemyQueueText;
+    public TMP_Text destroyableQueueText;
+    public TMP_Text forageableQueueText;
+
+    private List<Dictionary<GameObject, int>> allObjectsToSpawn;
+
+
     void Start()
     {
-        
+        enemyDict = new Dictionary<GameObject, int>();
+        destroyableDict = new Dictionary<GameObject, int>();
+        forageableDict = new Dictionary<GameObject, int>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(debugKey) && debugScreen != null 
-            && !CookingManager.Singleton.IsCooking()){
-                debugging = !debugging;
-                if(debugging){
-                    openDebugMenu();
-                } else {
-                    closeDebugMenu();
-                }
-            } 
-            
+        if (Input.GetKeyDown(debugKey) && debugScreen != null
+            && !CookingManager.Singleton.IsCooking())
+        {
+            debugging = !debugging;
+            if (debugging)
+            {
+                openDebugMenu();
+            }
+            else
+            {
+                closeDebugMenu();
+            }
+        }
+
     }
 
-    public void openDebugMenu(){
+    public void openDebugMenu()
+    {
         debugging = true;
         Time.timeScale = 0;
         CursorManager.Singleton.cursorObject.SetActive(false);
@@ -46,7 +74,8 @@ public class CombatTestRoom : MonoBehaviour
         PlayerEntityManager.Singleton.input.Disable();
     }
 
-    public void closeDebugMenu(){
+    public void closeDebugMenu()
+    {
         debugging = false;
         Time.timeScale = 1;
         CursorManager.Singleton.cursorObject.SetActive(true);
@@ -54,10 +83,124 @@ public class CombatTestRoom : MonoBehaviour
         PlayerEntityManager.Singleton.input.Enable();
     }
 
-    public void spawnAllObjects(){
+    public void spawnAllObjects()
+    {
         //loop through and spawn things at respective locations
     }
-    
+
+    public void plusEnemy()
+    {
+        Int32.TryParse(enemyInputNumber.text, out int temp);
+        if (temp >= 99)
+        {
+            temp = 99;
+        }
+        else
+        {
+            temp += 1;
+        }
+        enemyInputNumber.text = temp.ToString();
+    }
+
+    public void plusDestoryable()
+    {
+        // enemyInputNumber.text -= 1;
+        Int32.TryParse(destroyableInputNumber.text, out int temp);
+        if (temp >= 99)
+        {
+            temp = 99;
+        }
+        else
+        {
+            temp += 1;
+        }
+        destroyableInputNumber.text = temp.ToString();
+
+    }
+
+    public void plusForageable()
+    {
+        Int32.TryParse(forageableInputNumber.text, out int temp);
+        if (temp >= 99)
+        {
+            temp = 99;
+        }
+        else
+        {
+            temp += 1;
+        }
+        forageableInputNumber.text = temp.ToString();
+
+    }
+
+    public void minusEnemy()
+    {
+        Int32.TryParse(enemyInputNumber.text, out int temp);
+        if (temp <= 0)
+        {
+            temp = 0;
+        }
+        else
+        {
+            temp -= 1;
+        }
+        enemyInputNumber.text = temp.ToString();
+    }
+
+    public void minusDestroyable()
+    {
+        Int32.TryParse(destroyableInputNumber.text, out int temp);
+        if (temp <= 0)
+        {
+            temp = 0;
+        }
+        else
+        {
+            temp -= 1;
+        }
+        destroyableInputNumber.text = temp.ToString();
+    }
+
+    public void minusForageable()
+    {
+        Int32.TryParse(forageableInputNumber.text, out int temp);
+        if (temp <= 0)
+        {
+            temp = 0;
+        }
+        else
+        {
+            temp -= 1;
+        }
+        forageableInputNumber.text = temp.ToString();
+    }
+
+    public void addEnemy()
+    {
+        //dropdown value and add that and amount to the dicitonary
+        string dropdownValue = enemyDropdown.options[enemyDropdown.value].text;
+        //Debug.Log(dropdownValue);
+        Int32.TryParse(enemyInputNumber.text, out int numberOf);
+        // Debug.Log(numberOf);
+        GameObject dropdownGO = enemyList[enemyDropdown.value];
+        //Debug.Log(dropdownGO);
+
+        enemyDict.Add(dropdownGO, numberOf);
+        updateEnemyQueueText();
+    }
+
+    public void updateEnemyQueueText()
+    {
+        //key - value
+        enemyQueueText.text = "";
+        foreach (KeyValuePair<GameObject, int> kvp in enemyDict)
+        {
+            //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            enemyQueueText.text += string.Format("{0}, {1}", kvp.Key, kvp.Value);
+        }
+        //Debug.Log(enemyDict.Values);
+    }
+
 
 
 }
