43f6f3e12c6a3410949c6a99cbf5cbed536acc03
Igor Bessa <ibessa@ucsc.edu>
2025-04-25 16:14:13 -0700

    Saving changes to files, trying to test dialogue in fully working scene

--- Assets/Interactables/NPC.cs
+++ Assets/Interactables/NPC.cs
@@ -7,19 +7,14 @@ public class NPC : Interactable
     [Header("NPC")]
     [SerializeField] private DialogueTrigger dialogueTrigger;
     [SerializeField] private bool repeatable = true; // if the NPC can be interacted with multiple times
-    private int _interactCount = 0; // how many times the NPC has been interacted with
 
     private void Awake()
     {
         // set the interactable to be highlighted
-        SetInteractable(true);
+        //SetInteractable(true);
     }
     public override void Interact()
     {
-        if(!repeatable && _interactCount > 0)
-        {
-            SetInteractable(false); // set the interactable to false so it can't be interacted with again
-        }
 
         if (CanInteract())
         {
@@ -33,7 +28,11 @@ public class NPC : Interactable
             {
                 Debug.LogWarning("DialogueTrigger is not assigned for NPC: " + gameObject.name);
             }
-            _interactCount++;
+            
+            if(!repeatable)
+            {
+                SetInteractable(false);
+            }
         }
     }
 
