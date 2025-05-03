49d07601ad42407c946e4c4cfb7daf02ca7467fb
Cheristic <ethanheffan@gmail.com>
2025-05-02 21:06:29 -0700

    prepped for steam build upload

--- Assets/Entities/Enemies/Flettuce/RootFlettuceState.cs
+++ Assets/Entities/Enemies/Flettuce/RootFlettuceState.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEditor.Rendering;
 using UnityEngine;
 
--- Assets/Helper/CombatTestRoom.cs
+++ Assets/Helper/CombatTestRoom.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using TMPro;
 using System.Linq;
