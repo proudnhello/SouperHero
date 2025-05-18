b21139192de00a3d17a6bcf3ff19a88bc1ff1a08
Jake Sales <jfsales@ucsc.edu>
2025-05-17 17:31:16 -0700

    Prepping control rebinds
    This is gonna be pain :)

---
+++ Assets/UI/Menus/Settings/PlayerKeybinds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerKeybinds : MonoBehaviour
+{
+    public static PlayerKeybinds instance;
+
+    public InputActionReference useSpoon;
+    public InputActionReference dash;
+    public InputActionReference interact;
+    public InputActionReference inventory;
+    public InputActionReference pause;
+
+    public InputActionReference cycleSpoonLeft;
+    public InputActionReference cycleSpoonRight;
+    public InputActionReference bowl1;
+    public InputActionReference bowl2;
+    public InputActionReference bowl3;
+    public InputActionReference bowl4;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+}
