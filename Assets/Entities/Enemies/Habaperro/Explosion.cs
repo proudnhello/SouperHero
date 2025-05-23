85eac5c09b987062cf99c70632e72d0182f24f97
proudnhello <92887967+proudnhello@users.noreply.github.com>
2025-05-23 16:24:34 -0700

    landmine

--- Assets/Entities/Enemies/Habaperro/Explosion.cs
+++ Assets/Entities/Enemies/Habaperro/Explosion.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Infliction = SoupSpoon.SpoonInfliction;
 
 public class Explosion : MonoBehaviour
 {
     public float timeActive = 2f;
     public float radius = 6f;
     public float radiusToCollider = .5f;
-    public int damage = 10;
     List<Entity> entitiesAffected = new();
 
-    void Start()
+    public void Explode(int damage = 10)
     {
         StartCoroutine(DestroyTimer());
         transform.localScale = new Vector3(radius, radius, radius);
@@ -26,7 +26,32 @@ public class Explosion : MonoBehaviour
                 if (!entitiesAffected.Contains(entity))
                 {
                     entitiesAffected.Add(entity);
-                    entity.ModifyHealth(-damage);
+                    entity.DealDamage(damage);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    public void Explode(List<Infliction> inflictions, float size = 6)
+    {
+        StartCoroutine(DestroyTimer());
+        transform.localScale = new Vector3(size, size, size);
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, size * radiusToCollider, CollisionLayers.Singleton.GetEntityLayer());
+        string s = "";
+        foreach (var col in cols)
+        {
+            s += col.name + " ";
+            try
+            {
+                Entity entity = col.GetComponent<Entity>();
+                if (!entitiesAffected.Contains(entity))
+                {
+                    entitiesAffected.Add(entity);
+                    entity.ApplyInfliction(inflictions, transform);
                 }
             }
             catch
